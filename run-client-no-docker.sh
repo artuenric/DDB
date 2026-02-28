5a92a39a38d5249057eaf666aadb9dda26dba50e
artuenric <artuenric@gmail.com>
2026-02-28 20:15:04 -0300

    refactor(docker)

--- run-client-no-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "=> Iniciando a Interface do Cliente DDB..."
-
-# Executa a classe DDBClient usando o classpath correto para Linux
-java -cp "out:lib/*" br.com.ddb.client.DDBClient
--- start-db-no-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd mysql-local
-echo "=> Iniciando MySQL Portátil..."
-LIB_DIR=$(dirname $(find $(pwd)/libaio_local -name "libaio.so.1" | head -n 1))
-LD_LIBRARY_PATH=$LIB_DIR ./bin/mysqld --defaults-file=./my.cnf &
-echo "=> Banco de dados subindo em segundo plano."
