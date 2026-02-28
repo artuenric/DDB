dd14eaf05c4bc7750296446d5fc6fc0b51107eb8
artuenric <artuenric@gmail.com>
2026-02-28 20:10:32 -0300

    feat(scripts): move scripts in dir

---
+++ scripts/start-db-no-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd mysql-local
+echo "=> Iniciando MySQL Portátil..."
+LIB_DIR=$(dirname $(find $(pwd)/libaio_local -name "libaio.so.1" | head -n 1))
+LD_LIBRARY_PATH=$LIB_DIR ./bin/mysqld --defaults-file=./my.cnf &
+echo "=> Banco de dados subindo em segundo plano."
